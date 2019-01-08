cdb86584b6c2ca406a78d50f8cf4df5c7f1fb779
罗明川 <gordon.luo@bytedance.com>
2019-01-08 11:20:52 +0800

    [update]: 高性能

--- lib/shooping_cart.dart
+++ lib/shooping_cart.dart
@@ -9,7 +9,7 @@ import './add_item_dialog.dart';
 class ShoppingCart extends StatelessWidget {
   ShoppingCart({Key key}) : super(key: key);
 
-  // 这里写不好容易导致整棵树的重绘，是效率的最大的问题，作者stupid了。
+  // 这里写不好容易导致整棵树的重绘，是效率的最大的问题，作者stupid了,或者是作者演示的时候完全不考虑性能。
   // @override
   // Widget build(BuildContext context) {
   //   return StoreConnector<ZenState, Store<ZenState>>(
